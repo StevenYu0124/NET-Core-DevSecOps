3c1d911a5f9cb706ea8347a8c36d98b7088555ed
Steven <e0390018@u.nus.edu>
2025-04-06 18:24:17 +0800

    create WeatherForecast controller

---
+++ NetCoreApis/WeatherForecast/WeatherForecastController.cs
@@ -0,0 +1,27 @@
+namespace NetCoreApis.WeatherForecast;
+
+using Microsoft.AspNetCore.Mvc;
+
+[Route("weatherforecast")]
+[ApiController]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    [HttpGet]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+        return forecast;
+    }
+}
